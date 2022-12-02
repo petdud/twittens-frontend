7117c731586bf2c9ca1465ef769ddd37aec3c16e
Petr Duda <petrduda@microsoft.com>
2022-12-02 15:02:13 +0100

    skeleton collections

--- src/components/collection-list/collection-list.tsx
+++ src/components/collection-list/collection-list.tsx
@@ -1,11 +1,20 @@
 import { useCollections } from "../../hooks/use-collections";
+import { CardSkeleton } from "../card/card";
 import { MainViewHeader } from "../main-view-header/main-view-header";
 import { SearchBar } from "../search-bar/search-bar";
 import { CollectionItem } from "./collection-item";
 
 
 export const CollectionList = () =>  {
-  const { data: collections, isLoading } = useCollections();
+  const { data: collections, isLoading, error } = useCollections();
+
+  if (error || (!isLoading && collections.length === 0)) {
+    return (
+      <div className="md:py-6 py-2 mb-12">
+        <div className="pb-4">Sorry, something went wrong 🫣</div>
+      </div>
+    )
+  }
 
   return (
     <div className="md:py-6 py-2 mb-12">
@@ -17,12 +26,20 @@ export const CollectionList = () =>  {
       </div>
       <div className="max-w-full px-4 sm:px-6 md:px-8 mx-5">
         <ul role="list" className="grid grid-cols-2 gap-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6">
-          {collections && collections?.map(({image, name, users, slug}) => {
-            const userTwitterCount = users.filter(user => user.twitter).length;
-            return ( 
-              <CollectionItem key={slug} description={`${userTwitterCount} members on Twitter`} image={image} name={name} path={`/collections/${slug}`} />
-            )
-          })}
+          {isLoading ? 
+            Array.from(Array(10)).map(i =>  (
+              <li key={i} className="flex justify-center relative top-0">
+                <CardSkeleton />
+              </li>
+            ))
+          :           
+            collections && collections?.map(({image, name, users, slug}) => {
+              const userTwitterCount = users.filter(user => user.twitter).length;
+              return ( 
+                <CollectionItem key={slug} description={`${userTwitterCount} members on Twitter`} image={image} name={name} path={`/collections/${slug}`} />
+              )
+            })
+          }
         </ul>
       </div>
     </div>
