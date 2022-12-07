385191de73408b903d47832093941f5f674bc0dc
Petr Duda <petrduda@microsoft.com>
2022-12-07 19:53:50 +0100

    fix build

--- src/components/collection-list/collection-list.tsx
+++ src/components/collection-list/collection-list.tsx
@@ -35,7 +35,7 @@ export const CollectionList = () =>  {
               </li>
             ))
           :           
-            collections && collections?.map(({isFeatured, image, name, users, slug}) => {
+            collections && collections?.map(({isFeatured, image, name, slug}) => {
               const collectionDetails = collectionsTwitterCounts.find(collectionCounts => collectionCounts.slug === slug);
               return ( 
                 <CollectionItem 
