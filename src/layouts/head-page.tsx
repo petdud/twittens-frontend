847bb0b85aa836ed02062026c108f53e4f47d7c0
Petr Duda <petrduda@microsoft.com>
2022-12-12 20:34:01 +0100

    umami

--- src/layouts/head-page.tsx
+++ src/layouts/head-page.tsx
@@ -11,6 +11,7 @@ export const HeadPage = ({title, description}: IHeadPageProps) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
+      <script async defer data-website-id="71f3f4a6-7fc8-4af2-b7c0-2b0e3ecbd5fd" src="https://twittens-lytics.up.railway.app/umami.js"></script>
     </Head>
   )
 }
