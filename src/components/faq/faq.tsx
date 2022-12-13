72cd5094192065cb9f7c1bf7a5140f46244c11f9
Petr Duda <petrduda@microsoft.com>
2022-12-13 14:13:00 +0100

    faq

--- src/components/faq/faq.tsx
+++ src/components/faq/faq.tsx
@@ -3,38 +3,38 @@ import { ChevronDownIcon } from '@heroicons/react/24/outline'
 import { GOOGLE_FORM_GET_LISTED, HOW_TO_ADD_TWITTER_TO_ENS, TWITTENS_TWITTER_URL } from '../../core/constants'
 import { classNames } from '../../utils'
 
-const LINK_CLASSNAME = "underline text-sky-800";
+const LINK_CLASSNAME = "underline text-sky-600 dark:text-sky-500";
 
 const faqs = [
   {
     id: "1",
     question: "How does Twittens works",
     answer:
-      "Displayed twitter accounts on our website are from collection owners who filled their twitter text record in their ENS names. The data are publicly available on the blockchain with user consent."
+      "Our website displays Twitter accounts of collection owners who have provided their Twitter handle in the primary ENS name associated with their collections. The information is publicly available on the blockchain with the user's consent"
   },
   {
     id: "2",
     question: "Can I get my NFT collection listed",
     answer:
-      <>Yes, please <a href={GOOGLE_FORM_GET_LISTED} className={LINK_CLASSNAME} rel="noreferrer" target="_blank">fill-in this form</a>. It might take us 2-4 weeks to add your collection. We might speed it up if you would like to contribute or get your collection featured: In that case, drop us an email on hi@twittens.xyz.</>
+      <>If you would like to have your NFT collection listed on Twittens, please <a href={GOOGLE_FORM_GET_LISTED} className={LINK_CLASSNAME} rel="noreferrer" target="_blank">fill out this form</a>. We will review your submission and add your collection to our website as soon as possible. If you have any questions or need assistance, please don&apos;t hesitate to contact us at <a href="mailto:hi@twittens.xyz" className={LINK_CLASSNAME}>hi@twittens.xyz</a>.</>
   },
   {
     id: "3",
     question: "Twitter accounts are missing in a collection",
     answer:
-      <>We update our data every 24 hours and the Twitter accounts are from the twitter text records from ENS names. <a href={HOW_TO_ADD_TWITTER_TO_ENS} className={LINK_CLASSNAME} rel="noreferrer" target="_blank">Here is a guide how to add a twitter account to an ENS name</a>.</>
+      <>If you notice that some Twitter accounts are missing from a collection on Twittens, please check that the corresponding collection owner has added their Twitter handle to the ENS name associated with their collection.<br/><br/>We regularly update our data from ENS text records, so if your Twitter handle is not included in your ENS name, it will not appear on our website. If you need help adding your Twitter handle to your ENS name, you can <a href={HOW_TO_ADD_TWITTER_TO_ENS} className={LINK_CLASSNAME} rel="noreferrer" target="_blank">refer to this guide</a>.</>
   },
   {
     id: "4",
     question: "I don't see my Twitter in a collection that I own",
     answer:
-      <>Please make sure your twitter account is added in your ENS name text records. <a href={HOW_TO_ADD_TWITTER_TO_ENS} className={LINK_CLASSNAME} rel="noreferrer" target="_blank">Here is a guide how to do it</a>. Once done, please wait up to 48 hours to be displayed in our website.</>
+      <>If you&apos;re the owner of a collection and you don&apos;t see your Twitter handle on Twittens, please check that your Twitter handle is included in the ENS name associated with your collection. You can easily add your Twitter handle to your ENS name using the our website <a href="https://set.twittens.xyz" className={LINK_CLASSNAME} rel="noreferrer" target="_blank">set.twittens.xyz</a>. <br/><br/>Once you&apos;ve added your Twitter handle to your ENS name, it may take up to 24 hours for it to appear on our website. If you need help or have any questions, please contact us: <a href="mailto:hi@twittens.xyz" className={LINK_CLASSNAME}>hi@twittens.xyz</a>.</>
   },
   {
     id: "5",
     question: "How can I contact you",
     answer:
-      <><a href={TWITTENS_TWITTER_URL} className={LINK_CLASSNAME} rel="noreferrer" target="_blank">Twitter</a>, Discord or drop us an email on hi@twittens.xyz</>
+      <>Discord, <a href={TWITTENS_TWITTER_URL} className={LINK_CLASSNAME} rel="noreferrer" target="_blank">Twitter</a>, or drop us an email on <a href="mailto:hi@twittens.xyz" className={LINK_CLASSNAME}>hi@twittens.xyz</a></>
   },
 ]
 
@@ -49,8 +49,8 @@ const FaqItem = ({question, answer}: IFaqItem) => {
       {({ open }) => (
         <>
           <dt className="text-lg">
-            <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400">
-              <span className="font-medium text-gray-900">{question}</span>
+            <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-400 dark:text-gray-200">
+              <span className="font-medium text-gray-900 dark:text-gray-100">{question}</span>
               <span className="ml-6 flex h-7 items-center">
                 <ChevronDownIcon
                   className={classNames(open ? '-rotate-180' : 'rotate-0', 'h-6 w-6 transform')}
@@ -60,7 +60,7 @@ const FaqItem = ({question, answer}: IFaqItem) => {
             </Disclosure.Button>
           </dt>
           <Disclosure.Panel as="dd" className="mt-2 pr-12">
-            <p className="text-base text-gray-500">{answer}</p>
+            <p className="text-base text-gray-500 dark:text-neutral-300">{answer}</p>
           </Disclosure.Panel>
         </>
       )}
@@ -71,11 +71,11 @@ const FaqItem = ({question, answer}: IFaqItem) => {
 export const Faq = () => {
   return (
     <div className="mx-auto max-w-7xl py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-3xl divide-y-2 divide-gray-200">
-        <h2 className="text-left text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+      <div className="mx-auto max-w-3xl divide-y-2 divide-gray-200 dark:divide-neutral-700">
+        <h2 className="text-left text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl dark:text-white">
           Frequently asked questions
         </h2>
-        <dl className="mt-6 space-y-6 divide-y divide-gray-200">
+        <dl className="mt-6 space-y-6 divide-y divide-gray-200 dark:divide-neutral-700">
           {faqs.map(({id, question, answer}) => (
             <FaqItem key={id} question={question} answer={answer} />
           ))}
