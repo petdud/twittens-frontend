31321bac4dc186370b59344b38bc74a4c4d3edba
Petr Duda <petrduda@microsoft.com>
2022-12-27 21:35:30 +0100

    fix url

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,5 +1,5 @@
-// export const BASE_API_URL = process.env.NEXT_PUBLIC_API_BACKEND_PROD;
-export const BASE_API_URL = "http://localhost:3333/";
+export const BASE_API_URL = process.env.NEXT_PUBLIC_API_BACKEND_PROD;
+// export const BASE_API_URL = "http://localhost:3333/";
 
 // paths in this application (twittens.xyz)
 export const LOCAL_API_PATHS = {
