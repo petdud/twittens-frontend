31540a6d2fa5107a3a0b1935ed894204a190741f
Petr Duda <petrduda@microsoft.com>
2023-01-15 15:16:40 +0100

    fix small screen

--- src/components/container/container.tsx
+++ src/components/container/container.tsx
@@ -8,9 +8,9 @@ interface IContainerProps  {
 export const Container = ({fullWidth, top = "medium", children}: IContainerProps) => (
   <div className={
     classNames(
-      top === "small" ? "pt-4" : "",
-      top === "medium" ? "pt-6" : "",
-      top === "large" ? "pt-10" : "",
+      top === "small" ? "pt-2 sm:pt-4" : "",
+      top === "medium" ? "pt-3 sm:pt-6" : "",
+      top === "large" ? "pt-5 sm:pt-10" : "",
       fullWidth ? 
         "max-w-full px-2 sm:px-6 md:px-8 mx-2 md:mx-5 mb-10" 
       : "mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 mb-10",
