ea7ccf1c99d389aacaa2556ae76c9cab90a6cd8d
Petr Duda <petrduda@microsoft.com>
2023-01-21 21:14:16 +0100

    pricing

--- src/components/pricing/pricing.tsx
+++ src/components/pricing/pricing.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from "next/image";
 import { CheckIcon } from '@heroicons/react/24/outline'
 import { classNames } from '../../utils';
 import { HOW_DOES_IT_WORK_CONTENT } from '../faq/faq';
@@ -87,7 +88,16 @@ export const Pricing = () => {
           </div>
           <div className="border-t border-gray-200 pt-16 xl:grid xl:grid-cols-3 xl:gap-x-8">
             <div>
-              <h2 className="text-lg font-semibold text-neutral-400">ENS + Twitter = Twittens ❤️</h2>
+              <div className="flex items-center gap-2 text-lg font-semibold text-neutral-400">
+                <Image
+                  className="h-8 w-auto"
+                  width="81"
+                  height="25"
+                  src="/twittens_symbol.png"
+                  alt="Twittens"
+                />
+                <span>ENS + Twitter = Twittens </span>
+              </div>
               <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100">Connect your community!</p>
               <p className="mt-4 text-lg text-gray-500 dark:text-gray-300">
                 Get together 10 community members to chip-in 0.01 ETH each and get listed on Twittens. Start building and connecting your community with the help of Twittens!
@@ -136,7 +146,7 @@ export const Pricing = () => {
       </div>
 
       {/* CTA section */}
-      <div className="bg-white dark:bg-black">
+      {/* <div className="bg-white dark:bg-black">
         <div className="mx-auto max-w-7xl py-12 px-6 lg:flex lg:items-center lg:justify-between lg:py-24 lg:px-8">
           <h2 className="text-3xl font-bold tracking-tight text-neutral-700 sm:text-4xl">
             <span className="block">Ready to be listed?</span>
@@ -155,7 +165,7 @@ export const Pricing = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
