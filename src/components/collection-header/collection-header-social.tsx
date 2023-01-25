8800d88c971358e7c2d971950e3a4a21b09aff75
Petr Duda <petrduda@microsoft.com>
2023-01-25 10:03:12 +0100

    fix bug with view collection

--- src/components/collection-header/collection-header-social.tsx
+++ src/components/collection-header/collection-header-social.tsx
@@ -18,31 +18,25 @@ export const CollectionHeaderSocial = ({
   externalUrl,
   twitter,
   slug
-}: ICollectionHeaderSocialProps) => {
-  if (!discord && !externalUrl && !twitter) {
-    return null;
-  }
-  
-  return (
-    <div className="flex items-center flex-row lg:flex-col lg:items-end">
-      <div className="flex lg:mt-1">
-        {twitter && 
-          <Link href={`https://www.twitter.com/${twitter}`} target="_blank" className="p-2 hover:bg-gray-200 rounded-md dark:hover:bg-gray-700">
-            <BsTwitter className="text-sky-500 dark:text-sky-300" />
-          </Link> 
-        }
-        {discord && 
-          <Link href={discord} target="_blank" className="p-2 hover:bg-gray-200 rounded-md dark:hover:bg-gray-700">
-            <FaDiscord className="text-indigo-800 dark:text-indigo-500"/>
-          </Link>
-        }
-        {externalUrl && 
-          <Link href={externalUrl} target="_blank" className="p-2 hover:bg-gray-200 rounded-md dark:hover:bg-gray-700">
-            <AiOutlineLink className="text-gray-700 dark:text-gray-300"/>
-          </Link>
-        }
-      </div>
-      <MarketplaceDropdownOptions name="View collection" address={address} appearance="transparent" slug={slug} />
-    </div>
-  )
-}
+}: ICollectionHeaderSocialProps) => (
+  <div className="flex items-center flex-row lg:flex-col lg:items-end">
+    {twitter || discord || externalUrl && <div className="flex lg:mt-1">
+      {twitter && 
+        <Link href={`https://www.twitter.com/${twitter}`} target="_blank" className="p-2 hover:bg-gray-200 rounded-md dark:hover:bg-gray-700">
+          <BsTwitter className="text-sky-500 dark:text-sky-300" />
+        </Link> 
+      }
+      {discord && 
+        <Link href={discord} target="_blank" className="p-2 hover:bg-gray-200 rounded-md dark:hover:bg-gray-700">
+          <FaDiscord className="text-indigo-800 dark:text-indigo-500"/>
+        </Link>
+      }
+      {externalUrl && 
+        <Link href={externalUrl} target="_blank" className="p-2 hover:bg-gray-200 rounded-md dark:hover:bg-gray-700">
+          <AiOutlineLink className="text-gray-700 dark:text-gray-300"/>
+        </Link>
+      }
+    </div>}
+    <MarketplaceDropdownOptions name="View collection" address={address} appearance="transparent" slug={slug} />
+  </div>
+)
