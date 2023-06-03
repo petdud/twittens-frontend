c91800798148088aa56e9a7a3842f79795f056bd
Petr Duda <petrduda@microsoft.com>
2023-06-03 14:07:33 +0200

    tweet count profile

--- src/components/user-preview-modal/user-preview-modal.tsx
+++ src/components/user-preview-modal/user-preview-modal.tsx
@@ -105,6 +105,7 @@ export const UserPreviewModal = ({open, onClose, user, collections}: IUserPrevie
                         <div className="flex gap-4 text-sm items-center">
                           <div className="flex dark:text-white flex-row gap-1"><span className="font-semibold">{twitter?.following.toLocaleString()}</span> <span className="text-gray-500 dark:text-gray-400 text-sm font-normal">Following</span></div>
                           <div className="flex dark:text-white flex-row gap-1"><span className="font-semibold">{twitter?.followers.toLocaleString()}</span> <span className="text-gray-500 dark:text-gray-400 text-sm font-normal">Followers</span></div>
+                          <div className="flex dark:text-white flex-row gap-1"><span className="font-semibold">{twitter?.tweetCount.toLocaleString()}</span> <span className="text-gray-500 dark:text-gray-400 text-sm font-normal">Tweets</span></div>
                         </div>
                         <FooterButtons address={address} twitterUsername={twitter?.username} />
                       </div>
