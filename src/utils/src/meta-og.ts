67a7a984787c2aeb526ae1b7c55f79548c2096db
Petr Duda <peta.duda@gmail.com>
2024-02-19 21:08:42 +0100

    images

--- src/utils/src/meta-og.ts
+++ src/utils/src/meta-og.ts
@@ -1,294 +1,294 @@
 export const META_OG = {
   permies: {
     name: 'Permies',
-    ogImage: 'https://twittens.xyz/op_meta/permies.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/permies.jpg'
   },
   cryptopunks: {
     name: 'CryptoPunks',
-    ogImage: 'https://twittens.xyz/op_meta/cryptopunks.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/cryptopunks.jpg'
   },
   boredapeyachtclub: {
     name: 'Bored Ape Yacht Club',
-    ogImage: 'https://twittens.xyz/op_meta/boredapeyachtclub.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/boredapeyachtclub.jpg'
   },
   'proof-collective': {
     name: 'PROOF Collective',
-    ogImage: 'https://twittens.xyz/op_meta/proof.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/proof.jpg'
   },
   'proof-moonbirds': {
     name: 'Moonbirds',
-    ogImage: 'https://twittens.xyz/op_meta/moonbirds.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/moonbirds.jpg'
   },
   'world-of-women-nft': {
     name: 'World of Women',
-    ogImage: 'https://twittens.xyz/op_meta/wow.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/wow.jpg'
   },
   '8liens': {
     name: '8liens',
-    ogImage: 'https://twittens.xyz/op_meta/8liens.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/8liens.jpg'
   },
   'mutant-ape-yacht-club': {
     name: 'Mutant Ape Yacht Club',
-    ogImage: 'https://twittens.xyz/op_meta/mayc.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/mayc.jpg'
   },
   akutars: {
     name: 'Akutars',
-    ogImage: 'https://twittens.xyz/op_meta/akutars.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/akutars.jpg'
   },
   acclimatedmooncats: {
     name: 'MoonCats',
-    ogImage: 'https://twittens.xyz/op_meta/mooncats.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/mooncats.jpg'
   },
   clonex: {
     name: 'Clone X',
-    ogImage: 'https://twittens.xyz/op_meta/clonex.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/clonex.jpg'
   },
   azuki: {
     name: 'Azuki',
-    ogImage: 'https://twittens.xyz/op_meta/azuki.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/azuki.jpg'
   },
   'doodles-official': {
     name: 'Doodles',
-    ogImage: 'https://twittens.xyz/op_meta/doodles.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/doodles.jpg'
   },
   'cool-cats-nft': {
     name: 'Cool Cats',
-    ogImage: 'https://twittens.xyz/op_meta/coolcats.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/coolcats.jpg'
   },
   'a0k1verse-passport': {
     name: 'A0k1verse Passport',
-    ogImage: 'https://twittens.xyz/op_meta/a0k1verse.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/a0k1verse.jpg'
   },
   digidaigaku: {
     name: 'DigiDaigaku Genesis',
-    ogImage: 'https://twittens.xyz/op_meta/digidaigaku.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/digidaigaku.jpg'
   },
   'world-of-women-galaxy': {
     name: 'World of Women Galaxy',
-    ogImage: 'https://twittens.xyz/op_meta/wowg.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/wowg.jpg'
   },
   'budverse-cans-heritage-edition': {
     name: 'Budverse Cans - Heritage Edition',
-    ogImage: 'https://twittens.xyz/op_meta/budverse.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/budverse.jpg'
   },
   supernormalbyzipcy: {
     name: 'SuperNormal',
-    ogImage: 'https://twittens.xyz/op_meta/supernormal.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/supernormal.jpg'
   },
   'ledger-market-pass-genesis-edition': {
     name: '[ Ledger ] Market Pass - Genesis Edition',
-    ogImage: 'https://twittens.xyz/op_meta/ledger.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/ledger.jpg'
   },
   boonjiproject: {
     name: 'Boonji Project',
-    ogImage: 'https://twittens.xyz/op_meta/boonji.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/boonji.jpg'
   },
   pudgypenguins: {
     name: 'Pudgy Penguins',
-    ogImage: 'https://twittens.xyz/op_meta/pudgy.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/pudgy.jpg'
   },
   veefriends: {
     name: 'VeeFriends',
-    ogImage: 'https://twittens.xyz/op_meta/veefriends.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/veefriends.jpg'
   },
   meebits: {
     name: 'Meebits',
-    ogImage: 'https://twittens.xyz/op_meta/meebits.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/meebits.jpg'
   },
   'genuine-undead': {
     name: 'Genuine Undead',
-    ogImage: 'https://twittens.xyz/op_meta/genuineundead.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/genuineundead.jpg'
   },
   'trump-digital-trading-cards': {
     name: 'Trump Digital Trading Cards',
-    ogImage: 'https://twittens.xyz/op_meta/trump.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/trump.jpg'
   },
   'armins-all-access': {
     name: "Armin's All-Access",
-    ogImage: 'https://twittens.xyz/op_meta/armin.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/armin.jpg'
   },
   mfers: {
     name: 'mfers',
-    ogImage: 'https://twittens.xyz/op_meta/mfers.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/mfers.jpg'
   },
   thememes6529: {
     name: 'The Memes by 6529',
-    ogImage: 'https://twittens.xyz/op_meta/thememes.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/thememes.jpg'
   },
   beanzofficial: {
     name: 'BEANZ Official',
-    ogImage: 'https://twittens.xyz/op_meta/beanz.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/beanz.jpg'
   },
   deadfellaz: {
     name: 'Deadfellaz',
-    ogImage: 'https://twittens.xyz/op_meta/deadfellaz.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/deadfellaz.jpg'
   },
   alienfrensnft: {
     name: 'alien frens',
-    ogImage: 'https://twittens.xyz/op_meta/alienfrens.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/alienfrens.jpg'
   },
   'nft-paris-23': {
     name: 'NFT Paris 2023',
-    ogImage: 'https://twittens.xyz/op_meta/nftparis.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/nftparis.jpg'
   },
   cdao: {
     name: 'CityDAO Citizenship',
-    ogImage: 'https://twittens.xyz/op_meta/citydao.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/citydao.jpg'
   },
   bcard2: {
     name: 'Bcard 2.0',
-    ogImage: 'https://twittens.xyz/op_meta/bcard.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/bcard.jpg'
   },
   'strv-web3-holiday-meetup': {
     name: 'STRV Web3 Holiday Meetup'
   },
   rektguy: {
     name: 'rektguy',
-    ogImage: 'https://twittens.xyz/op_meta/rektguy.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/rektguy.jpg'
   },
   curiocardswrapper: {
     name: 'My Curio Cards',
-    ogImage: 'https://twittens.xyz/op_meta/curiocards.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/curiocards.jpg'
   },
   goblintownwtf: {
     name: 'goblintown.wtf',
-    ogImage: 'https://twittens.xyz/op_meta/goblintown.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/goblintown.jpg'
   },
   autoglyphs: {
     name: 'Autoglyphs',
-    ogImage: 'https://twittens.xyz/op_meta/autoglyphs.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/autoglyphs.jpg'
   },
   womenrise: {
     name: 'WomenRise',
-    ogImage: 'https://twittens.xyz/op_meta/womenrise.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/womenrise.jpg'
   },
   otherdeed: {
     name: 'Otherdeed for Otherside',
-    ogImage: 'https://twittens.xyz/op_meta/otherdeed.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/otherdeed.jpg'
   },
   terraforms: {
     name: 'Terraforms by Mathcastles',
-    ogImage: 'https://twittens.xyz/op_meta/terraforms.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/terraforms.jpg'
   },
   thepotatoz: {
     name: 'The Potatoz',
-    ogImage: 'https://twittens.xyz/op_meta/potatoz.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/potatoz.jpg'
   },
   evmavericks: {
     name: 'EVMavericks',
-    ogImage: 'https://twittens.xyz/op_meta/evmavericks.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/evmavericks.jpg'
   },
   nouns: {
     name: 'Nouns',
-    ogImage: 'https://twittens.xyz/op_meta/nouns.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/nouns.jpg'
   },
   thesaudis: {
     name: 'The Saudis',
-    ogImage: 'https://twittens.xyz/op_meta/saudis.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/saudis.jpg'
   },
   'right-click-share': {
     name: 'Right Click Share by XCOPY and Deca',
-    ogImage: 'https://twittens.xyz/op_meta/rcs.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/rcs.jpg'
   },
   'blueberry-club': {
     name: 'GMX Blueberry Club',
-    ogImage: 'https://twittens.xyz/op_meta/blueberry.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/blueberry.jpg'
   },
   'non-fungible-conference-2023': {
     name: 'Non Fungible Conference 2023',
-    ogImage: 'https://twittens.xyz/op_meta/nonfungibleconference.jpeg'
+    ogImage: 'https://xfrens.xyz/op_meta/nonfungibleconference.jpeg'
   },
   'vv-checks': {
     name: 'Checks - VV Edition',
-    ogImage: 'https://twittens.xyz/op_meta/checks.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/checks.jpg'
   },
   partydegenerates: {
     name: 'Party Degenerates',
-    ogImage: 'https://twittens.xyz/op_meta/partydegens.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/partydegens.jpg'
   },
   ensmaxis: {
     name: 'ENS Maxis',
-    ogImage: 'https://twittens.xyz/op_meta/ensmaxis.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/ensmaxis.jpg'
   },
   'nessgraphics-open-editions': {
     name: 'NessGraphics Open Editions',
-    ogImage: 'https://twittens.xyz/op_meta/nessgraphics.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/nessgraphics.jpg'
   },
   'chromie-squiggle-by-snowfro': {
     name: 'Chromie Squiggle',
-    ogImage: 'https://twittens.xyz/op_meta/chromie.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/chromie.jpg'
   },
   'fidenza-by-tyler-hobbs': {
     name: 'Fidenza',
-    ogImage: 'https://twittens.xyz/op_meta/fidenza.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/fidenza.jpg'
   },
   'ringers-by-dmitri-cherniak': {
     name: 'Ringers',
-    ogImage: 'https://twittens.xyz/op_meta/ringers.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/ringers.jpg'
   },
   'gazers-by-matt-kane': {
     name: 'Gazers',
-    ogImage: 'https://twittens.xyz/op_meta/gazers.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/gazers.jpg'
   },
   'memories-of-qilin-by-emily-xie': {
     name: 'Memories of Qilin',
-    ogImage: 'https://twittens.xyz/op_meta/memories.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/memories.jpg'
   },
   'the-harvest-by-per-kristian-stoveland': {
     name: 'The Harvest',
-    ogImage: 'https://twittens.xyz/op_meta/theharvest.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/theharvest.jpg'
   },
   '0xai-genesis': {
     name: '0xAI',
-    ogImage: 'https://twittens.xyz/op_meta/0xai.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/0xai.jpg'
   },
   'genesis-by-claire-silver': {
     name: 'Genesis by Claire Silver',
-    ogImage: 'https://twittens.xyz/op_meta/genesis.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/genesis.jpg'
   },
   'podgans-by-pindar-van-arman': {
     name: 'podGANs',
-    ogImage: 'https://twittens.xyz/op_meta/podgans.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/podgans.jpg'
   },
   'brain-loops-by-gene-kogan': {
     name: 'Brain Loops',
-    ogImage: 'https://twittens.xyz/op_meta/brainloops.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/brainloops.jpg'
   },
   'life-in-west-america-by-roope-rainisto': {
     name: 'Life In West America',
-    ogImage: 'https://twittens.xyz/op_meta/lifeinwestamerica.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/lifeinwestamerica.jpg'
   },
   'chimerical-stories-by-entangled-others-sofia-cresp': {
     name: 'Chimerical Stories',
-    ogImage: 'https://twittens.xyz/op_meta/chimerical_stories.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/chimerical_stories.jpg'
   },
   cryptoarte: {
     name: 'CryptoArte',
-    ogImage: 'https://twittens.xyz/op_meta/cryptoarte.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/cryptoarte.jpg'
   },
   nowpass: {
     name: 'Now Pass',
-    ogImage: 'https://twittens.xyz/op_meta/nowpass.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/nowpass.jpg'
   },
   y00ts: {
     name: 'Y00ts',
-    ogImage: 'https://twittens.xyz/op_meta/y00ts.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/y00ts.jpg'
   },
   degods: {
     name: 'DeGods',
-    ogImage: 'https://twittens.xyz/op_meta/degods.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/degods.jpg'
   },
   'the-devs-nft': {
     name: "The Dev's NFT",
-    ogImage: 'https://twittens.xyz/op_meta/thedev.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/thedev.jpg'
   },
   'opepen-edition': {
     name: 'Opepen Edition',
-    ogImage: 'https://twittens.xyz/op_meta/opepen.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/opepen.jpg'
   },
   'friendship-bracelets-by-alexis-andre': {
     name: 'Friendship Bracelets',
-    ogImage: 'https://twittens.xyz/op_meta/friendship-bracelets.jpg'
+    ogImage: 'https://xfrens.xyz/op_meta/friendship-bracelets.jpg'
   },
   bytegans: {
     name: 'byteGANs',
