b65bcb40fe50e8febcf017ce57379a0f504cfbaa
Petr Duda <peta.duda@gmail.com>
2025-02-08 21:47:47 +0100

    a

--- src/hooks/use-lens-profile.tsx
+++ src/hooks/use-lens-profile.tsx
@@ -1,27 +1,38 @@
-// import React from "react";
-// import { Profile, useProfile } from "@lens-protocol/react-web";
-// import { ILensProfile } from "../core/collection.interface";
+import React from 'react';
+import { Profile, useProfile } from '@lens-protocol/react-web';
+import { ILensProfile } from '../core/collection.interface';
 
-// export type GeneralLensProfile = ILensProfile | Profile | null | undefined;
+export type GeneralLensProfile = ILensProfile | Profile | null | undefined;
 
-// // Type guard to check if the value is of type Profile
-// export function isProfile(profile: GeneralLensProfile): profile is Profile {
-//   return profile !== null && profile !== undefined && (profile as Profile).followStatus !== undefined;
-// }
+// Type guard to check if the value is of type Profile
+export function isProfile(profile: GeneralLensProfile): profile is Profile {
+  return (
+    profile !== null &&
+    profile !== undefined &&
+    (profile as any).followStatus !== undefined
+  );
+}
 
-// export const useLensProfile = (storedProfile: ILensProfile | null) => {
-//     let { data: fetchedProfile, loading } = useProfile({ handle: storedProfile?.handle || "" });
+export const useLensProfile = (storedProfile: ILensProfile | null) => {
+  let { data: fetchedProfile, loading } = useProfile({
+    forHandle: storedProfile?.handle || '' // it has been changed
+  });
 
-//     const isProfileFetched = storedProfile && fetchedProfile && storedProfile?.id === fetchedProfile?.id && !loading;
+  const isProfileFetched =
+    storedProfile &&
+    fetchedProfile &&
+    storedProfile?.id === fetchedProfile?.id &&
+    !loading;
 
-//     const profile = React.useMemo(
-//       () => isProfileFetched ? fetchedProfile : storedProfile,
-//     [fetchedProfile, isProfileFetched, storedProfile]);
+  const profile = React.useMemo(
+    () => (isProfileFetched ? fetchedProfile : storedProfile),
+    [fetchedProfile, isProfileFetched, storedProfile]
+  );
 
-//     return {
-//       storedProfile,
-//       fetchedProfile,
-//       profile,
-//       loading
-//     };
-// };
+  return {
+    storedProfile,
+    fetchedProfile,
+    profile,
+    loading
+  };
+};
